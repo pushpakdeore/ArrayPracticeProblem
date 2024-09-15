de0fb7297219a8529f122cbe4bc1885311063e20
Pushpak Deore <110999787+pushpakdeore@users.noreply.github.com>
2024-09-16 00:13:04 +0530

    Update java19Element_sum_equal_target.java

--- java19Element_sum_equal_target.java
+++ java19Element_sum_equal_target.java
@@ -1,6 +1,8 @@
 package pushpak11Array;
 
 public class java19Element_sum_equal_target {
+    //ok
+    
     public static void main(String[] args) {
         int[] array  = {1,2,3,4,5,6,7,};
         int target = 5;
